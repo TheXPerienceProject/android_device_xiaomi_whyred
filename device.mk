eab43a5863b5ee240491ae81617beb7a923a80ef
klozz <shaclone707@gmail.com>
2018-07-23 06:36:13 -0400

    whyred:bring some xpe love
    Signed-off-by: klozz <shaclone707@gmail.com>

--- device.mk
+++ device.mk
@@ -122,6 +122,7 @@ PRODUCT_PACKAGES += \
     libhwc2on1apadapter \
     libqdMetaData.system \
     libtinyxml
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
